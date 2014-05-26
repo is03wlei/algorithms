5d38dee9e7b90edb6370f6c48b806b519b9b0107
Lei Lang <Lei Wang>
2014-05-26 17:55:28 +0800

    fix bug for cutTree

--- csdn/cutTree.cpp
+++ csdn/cutTree.cpp
@@ -3,19 +3,25 @@
 #include <iostream>
 #include <cstdio>
 #include <vector>
+#include <cstring>
 #include <algorithm>
-
 using namespace std;
 
-int dfs(const int &fromNode,const vector<int> &weight,const vector<vector<int> > &edge,
-		vector<bool> &visit, vector<int> &subTreeSum)
+const int MAX_NODES=100000;
+int weight[MAX_NODES];
+int subTreeSum[MAX_NODES];
+bool visit[MAX_NODES];
+vector<vector<int> > edge(MAX_NODES);
+
+
+int dfs(const int &fromNode)
 {
 	if (visit[fromNode]) return 0;
 	visit[fromNode]=true;
 	subTreeSum[fromNode]=weight[fromNode];
 	for(int i=0;i<edge[fromNode].size();i++){
 		int toNode=edge[fromNode][i];
-		subTreeSum[fromNode]+=dfs(toNode,weight,edge,visit,subTreeSum);
+		subTreeSum[fromNode]+=dfs(toNode);
 	}
 	return subTreeSum[fromNode];
 }
@@ -26,13 +32,12 @@ int main()
 
 	while(scanf("%d",&nodeNum)!=EOF){
 		int weightSum=0;
-		vector<int> weight(nodeNum);
 		for(i=0;i<nodeNum;i++){
 			scanf("%d",&weight[i]);
 			weightSum+=weight[i];
 		}
 
-		vector<vector<int> > edge(nodeNum);
+		edge.clear();
 
 		for(i=1;i<nodeNum;i++){
 			int fromNode,toNode;
@@ -43,10 +48,10 @@ int main()
 			edge[toNode].push_back(fromNode);
 		}
 		
-		vector<bool> visit(nodeNum);
-		vector<int> subTreeSum(nodeNum);
+		memset(visit,false,sizeof(bool)*nodeNum);
+		//for(i=0;i<nodeNum;i++)cout<<visit[i]<<endl;
 
-		dfs(0,weight,edge,visit,subTreeSum);
+		dfs(0);
 
 		//printf("%d\n",weightSum);
 
